5e2b3375f6a5865611b3db1c754f12c444bac4d8
Luis Goenaga <luis.goenaga@est.iudigital.edu.co>
2024-10-28 10:30:34 -0500

    SQLite y Configuración

---
+++ config/database.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+sqlite_db = "database"
+base_dir = os.path.dirname(os.path.relpath(__file__))
+
+database_url = f"sqlite:///{os.path.join(base_dir, sqlite_db)}"
+
+engine =  create_engine(database_url,  echo=True)
+
+session =  sessionmaker(bind=engine)
+
+db_tb = declarative_base()
